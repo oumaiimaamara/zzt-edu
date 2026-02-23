9a04ce88a7a9e553246f114e424f5b669365dab2
Oumaima Amara <36845327+oumaiimaamara@users.noreply.github.com>
2026-02-23 20:13:25 +0100

    add verifyToken export

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,41 @@
+
 import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+
+const JWT_SECRET = process.env.JWT_SECRET!;
+
+export function signToken(payload: {
+  userId: string;
+  email: string;
+  role: string;
+}) {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
+}
+
+export function verifyToken(token: string) {
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as any;
+
+    return {
+      id: decoded.userId,
+      email: decoded.email,
+      role: decoded.role,
+    };
+  } catch {
+    return null;
+  }
+}
+
+export async function getUserFromCookies() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("auth_token")?.value;
+
+  if (!token) return null;
+
+  return verifyToken(token);
+}
+
+/*import jwt from "jsonwebtoken";
 import { cookies } from "next/headers";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
@@ -32,4 +69,4 @@ export async function getUserFromCookies(): Promise<{
   } catch {
     return null;
   }
-}
+}*/
